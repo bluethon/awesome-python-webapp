eb49f811a370fd69789eeee1743f8928c0c410d4
bluethon <j5088794@gmail.com>
2015-06-30 17:23:31 +0800

    update www/schema.sql 20150630

--- www/schema.sql
+++ www/schema.sql
@@ -6,18 +6,21 @@ create database awesome;
 
 use awesome;
 
+-- grant 把指定的权限分配给特定的用户，如果这个用户不存在，则会创建一个用户
+-- 命令格式
+-- grant 权限 on 数据库名.表名 to 用户名@登陆方式 identified by 'password1';
 grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
 
-create table users{
+create table users (
     `id` varchar(50) not null,
     `email` varchar(50) not null,
     `passwd` varchar(50) not null,
-    `admin` bool not null, 
+    `admin` bool not null,
     `name` varchar(50) not null,
     `image` varchar(500) not null,
     `created_at` real not null,
     unique key `idx_email` (`email`),
     key `idx_created_at` (`created_at`),
     primary key (`id`)
-} engine=innodb default charset=utf8
+) engine=innodb default charset=utf8
 
